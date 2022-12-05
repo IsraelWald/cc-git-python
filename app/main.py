7a6e0834990eef8de9711507cc99dc98531803b3
Israel Waldner <israelw@tenorconsulting.net>
2022-12-05 13:54:18 +0000

    Added cat-file command

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 import sys
 import os
+import zlib
+
+
+def cat_file(args):
+    folder = args[:2]
+    filename = args[2:]
+    fp = os.path.join(".git", "objects", folder, filename)
+    with open(fp, "rb") as f:
+        content = zlib.decompress(f.read())
+        _, content = content.split(b'\x00')
+        print(content, end="")
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,6 +22,9 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        cat_file(sys.argv[3])
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
